097799bedbe4c53d758a4e783758f70325b1530b
Jay Bhakhar <jaybhakhar@gmail.com>
2022-04-08 17:54:23 +0500

    version1

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.render("dashboard");
+  res.render("home");
 });
 
+mongoose.connect(
+  "mongodb+srv://jaybhakhar:jay456789@quiz-model.eiqfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+const quizModelSchema = {
+  name: String,
+  budget: String,
+  number: String,
+};
+
+const Quiz_Model = mongoose.model("Quiz-Model", quizModelSchema);
+
+app.post("/", (req, res) => {
+  const quizModel = new Quiz_Model({
+    name: req.body.name,
+    budget: req.body.budget,
+    number: req.body.number,
+  });
+
+  quizModel.save(function (err) {
+    if (!err) {
+      res.redirect("/thankyou");
+    }
+  });
+});
+app.get("/dashboard", (req, res) => {
+  Quiz_Model.find({}, function (err, quiz_models) {
+    res.render("dashboard", {
+      quiz_models: quiz_models,
+    });
+  });
+});
+
+app.get("/dashboard/delete/:id", function (req, res) {
+  Quiz_Model.findByIdAndRemove(req.params.id, function (err) {
+    if (!err) {
+      res.redirect("/dashboard");
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/thankyou", (req, res) => {
+  res.render("thankyou");
+});
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express is working on port 3000");
 });
